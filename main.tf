70b9e9be812e6c4875f4877a7a5bff9271c11abd
ifzal-ahmed-npl <ifzal.ahmed@nowpensions.com>
2025-12-12 10:40:34 +0000

    fix: updated tag regex and added test source module

--- main.tf
+++ main.tf
@@ -1,3 +1,7 @@
 resource "aws_s3_bucket" "test_bucket" {
   bucket = "${var.environment}-ifzal-test-bucket-2k25"
 }
+
+module "test-s3-bucket" {
+  source = source = "git::https://github.com/NOW-Pensions/npl-global-terraform-modules.git//ecr?ref=DEVOPS-331-ecr-module"
+}
