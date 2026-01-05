e73909305171c68cdaf9d40aae5f0604087fbe14
ifzal-ahmed-npl <117187694+ifzal-ahmed-npl@users.noreply.github.com>
2026-01-05 10:44:58 +0000

    Added shared-deploy.yml

--- main.tf
+++ main.tf
@@ -1,7 +1,3 @@
 resource "aws_s3_bucket" "test_bucket" {
   bucket = "${var.environment}-ifzal-test-bucket-2k25"
 }
-
-module "test-s3-bucket" {
-  source = source = "git::https://github.com/NOW-Pensions/npl-global-terraform-modules.git//ecr?ref=v1.0.20"
-}
